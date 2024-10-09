25c82aa803694e5f22958a053045e4f4b453f84f
Maria Camila Orozco <mcor2437@gmail.com>
2024-10-08 22:53:22 -0500

    Feat: Add some routes

---
+++ src/PaginaBusqueda/PaginaDeBusqueda.jsx
@@ -0,0 +1,10 @@
+
+const PaginaDeBusqueda = () => {
+  return (
+    <div>
+      <h1>logica de busqueda // mirar en notas 1 1 2  </h1>
+    </div>
+  );
+};
+
+export default PaginaDeBusqueda;
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import NavBar from "./assets/Components/NavBar/NavBar";
 import Footer from "./assets/Components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PaginaDeBusqueda from "./PaginaDeBusqueda/PaginaDeBusqueda";
 
 createRoot(document.getElementById("root")).render(
-  <div className="container">
-    <NavBar />
-    <main>
-      {/*Photos */}
-      {/* <div>*Catalog /*/}
-    </main>
-    <Footer />
-  </div>
+  <StrictMode>
+    <BrowserRouter>
+      <div className="container">
+        <NavBar />
+        <main>
+          <Routes>
+            {/* en path coloque como va a salir en la url tipo https etc etc/comolopuseenelPath y en element el componente */}
+            <Route path="/busqueda" element={<PaginaDeBusqueda />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  </StrictMode>
 );
